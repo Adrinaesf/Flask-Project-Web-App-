1d825a49206d34f32e120610b9c6f927d0a123dd
Adrina <adrinacad.esf@gmail.com>
2026-01-07 22:16:11 -0500

    Added the notes for the template folder

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from website import creat_app
 app = creat_app()
 if __name__ == '__main__':
     # This runs the program:
-    # And debug and make changes as if we make any change. 
+    # And debug and make changes as if we make any change.
+    print("Starting Flask app...") 
     app.run(debug=True)
 
