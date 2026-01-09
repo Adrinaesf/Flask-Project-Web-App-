8416f3c37980adc8cf55413984b2c69fd00fc0b6
Adrina <adrinacad.esf@gmail.com>
2026-01-09 15:48:06 -0500

    Added the functionality to the buttons

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 # This has so much routs here, has URL, 
 
 auth = Blueprint('auth', __name__) # Recc: call the same name
@@ -6,12 +6,12 @@ auth = Blueprint('auth', __name__) # Recc: call the same name
 # We now create more routs: login, logout, sign up:
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>logout</p>"
+    return render_template("home.html")
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("signUp.html")
